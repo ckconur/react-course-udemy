ea52a6dd3bd9f3d9408646aa10c2df04e68f61d2
ckconur <cekiconurr@gmail.com>
2021-12-14 10:08:49 +0300

    inline conditional styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  // States
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  // Event Handlers
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    } else {
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
+    <>
+      <form onSubmit={formSubmitHandler}>
+        <div className="form-control">
+          <label style={{ color: !isValid ? "red" : "black" }}>
+            Course Goal
+          </label>
+          <input
+            style={{
+              borderColor: !isValid ? "red" : "#ccc",
+              background: !isValid ? "salmon" : "transparent",
+            }}
+            type="text"
+            onChange={goalInputChangeHandler}
+          />
+        </div>
+        <Button type="submit">Add Goal</Button>
+      </form>
+    </>
   );
 };
 
